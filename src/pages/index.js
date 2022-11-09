76a7a85503c708f4d4f8794488e0f992adb7d8c0
Juan Scanlan <juanscanlan@gmail.com>
2022-11-09 16:31:15 +0100

    add getServerSideProps to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,17 @@ import { useTranslation } from "next-i18next";
 import BaseLayout from "@core/Layout/BaseLayout/BaseLayout";
 
 // Locale gets the current language from the url, given the language codes in next-i18next.config.js
-export async function getStaticProps({ locale }) {
+export async function getServerSideProps({ locale, req }) {
+  const response = await fetch(
+    process.env.NEXT_PUBLIC_APP_SERVICES_URL + '/customer/view',
+    { method: 'GET', headers: { Cookie: req.headers.cookie } }
+  );
+  const { userid } = await response.json();
   return {
     props: {
       // Grab the translations from the files set in the square brackets, located in public/locales/*/
       ...(await serverSideTranslations(locale, ["home", "common"])),
+      userId: userid ?? null
     },
   };
 }
